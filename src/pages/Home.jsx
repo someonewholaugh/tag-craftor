87ef1ea89b4a84672ba509b2e900b1f622cd82ba
Rafif Athallah <rafifathallah99@gmail.com>
2024-10-29 18:49:57 +0700

    initial project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/layout/layout';
+
+const Home = () => {
+    return (
+        <Layout>
+            <h1>Home</h1>
+            <p>Welcome to the home page!</p>
+        </Layout>
+    );
+};
+
+export default Home;
