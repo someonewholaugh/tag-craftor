ef3277d360f53beb7a04bc1a81fb050b84370b48
Rafif Athallah <rafifathallah99@gmail.com>
2024-11-18 23:21:24 +0700

    feat: add cryptojs and minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,18 @@ import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './index.html',
-        './src/**/*.{js,ts,jsx,tsx}',
-        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                epilogue: ['Epilogue', 'sans-serif'],
-            },
-        },
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
     },
-    darkMode: 'class',
-    plugins: [nextui()],
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
 };
